03fdd7645f655c087a31ab3d7945579c8c4bf11a
James Draper <jamesdrape@gmail.com>
2020-01-10 11:05:23 +0000

    link to core channel video chat

--- handler.js
+++ handler.js
@@ -3,11 +3,11 @@
 const sendMessage = require('./lib/sendMessage'); 
 //const botChannelId = process.env.BOT_CHANNEL_ID; 
 const coreChannelId = process.env.CORE_CHANNEL_ID; 
-const standupChannelId = process.env.STANDUP_CHANNEL_ID;
+//const standupChannelId = process.env.STANDUP_CHANNEL_ID;
 
 module.exports.standup = async event => {
   try {
-    await sendMessage(`@online standup in 5 mins: https://digitalcrew.teamwork.com/call/chat/${standupChannelId}`, coreChannelId);
+    await sendMessage(`@online standup in 5 mins: https://digitalcrew.teamwork.com/call/chat/${coreChannelId}`, coreChannelId);
     return {
       statusCode: 200,
       body: JSON.stringify(
