8182920dd0278659c0c5e4bcb8102b8271a5b9b2
James Draper <jamesdrape@gmail.com>
2020-05-13 11:03:51 +0100

    Use new Standup Bot and post to Core Team channel

--- handler.js
+++ handler.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const sendMessage = require('./lib/sendMessage'); 
-const customFieldsDevsChannelUrl = 'https://chat-hooks.us.teamwork.com/v1/in/1/52ed7609-7161-460f-8745-d4b319e6eec1';
-//const coreChannelUrl = '';
+//const customFieldsDevsChannelUrl = 'https://chat-hooks.us.teamwork.com/v1/in/1/52ed7609-7161-460f-8745-d4b319e6eec1';
+const coreChannelUrl = 'https://chat-hooks.us.teamwork.com/v1/in/1/8f7a24d5-f6c5-45f9-a60c-b579d0bf4838';
 const coreChannelId = process.env.CORE_CHANNEL_ID; 
 
 module.exports.standup = async event => {
   try {
-    await sendMessage(`@online standup in 5 mins: https://digitalcrew.teamwork.com/call/chat/${coreChannelId}`, customFieldsDevsChannelUrl);
+    await sendMessage(`@online standup in 5 mins: https://digitalcrew.teamwork.com/call/chat/${coreChannelId}`, coreChannelUrl);
     return {
       statusCode: 200,
       body: JSON.stringify(
