a8ec7dce38eac6086b4eaa388dd7e73e34df6570
James Draper <jamesdrape@gmail.com>
2021-04-20 12:01:21 +0100

    move to new channel

--- handler.js
+++ handler.js
@@ -2,13 +2,13 @@
 
 const sendMessage = require('./lib/sendMessage');
 //const customFieldsDevsChannelUrl = 'https://chat-hooks.us.teamwork.com/v1/in/1/52ed7609-7161-460f-8745-d4b319e6eec1';
-const coreChannelUrl = 'https://chat-hooks.us.teamwork.com/v1/in/1/8f7a24d5-f6c5-45f9-a60c-b579d0bf4838';
-const coreChannelId = process.env.CORE_CHANNEL_ID;
+//const coreChannelUrl = 'https://chat-hooks.us.teamwork.com/v1/in/1/8f7a24d5-f6c5-45f9-a60c-b579d0bf4838';
+const standupChannelUrl = 'https://chat-hooks.us.teamwork.com/v1/in/1/eff5224e-6dd2-4e04-b9b5-db04906f4db4';
 const standupCallUrl = 'https://meet.google.com/yuk-dpcv-xgn'
 
 module.exports.standup = async event => {
   try {
-    await sendMessage(`@online standup in 5 mins - ${standupCallUrl}`, coreChannelUrl);
+    await sendMessage(`@online standup in 5 mins - :notebook: feed me some notes - ${standupCallUrl}`, standupChannelUrl);
     return {
       statusCode: 200,
       body: JSON.stringify(
